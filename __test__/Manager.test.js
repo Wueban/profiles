4937b4790a30289c79b4bf936f5293ac2f70fef3
felipe <angelvelandia1815@gmail.com>
2022-08-18 16:52:40 -0400

    office number test

---
+++ __test__/Manager.test.js
@@ -0,0 +1,8 @@
+
+const Manager = require('../lib/Manager');
+
+// test, manager object
+test('get manager office', () =>{
+    //manager office 
+    const manager = new Manager('camilo', 30, 'camilo12@gmail.com', 1)
+})
